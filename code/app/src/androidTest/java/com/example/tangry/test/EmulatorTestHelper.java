70f5bbe41679d3fa6596aac7878b7c5bab0e9cb2
Jumble28 <112910222+Jumble28@users.noreply.github.com>
2025-03-09 21:36:49 -0600

    completed tests

--- code/app/src/androidTest/java/com/example/tangry/test/EmulatorTestHelper.java
+++ code/app/src/androidTest/java/com/example/tangry/test/EmulatorTestHelper.java
@@ -7,6 +7,7 @@ import com.google.firebase.auth.FirebaseAuth;
 public class EmulatorTestHelper {
     private static final String EMULATOR_HOST = "10.0.2.2";
     private static final int FIRESTORE_PORT = 8080;
+    private static final int AUTH_PORT = 9099;
 
     public static void useFirebaseEmulators() {
         // Configure Firestore
@@ -17,5 +18,8 @@ public class EmulatorTestHelper {
                 .setPersistenceEnabled(false)
                 .build();
         firestore.setFirestoreSettings(settings);
+        // Configure Auth
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        auth.useEmulator(EMULATOR_HOST, AUTH_PORT);
     }
 }
